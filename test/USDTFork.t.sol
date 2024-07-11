f202b90f368f702f14ed03c622c2dc4cc7966eba
DJ.Zhang <dengji@evg.co>
2024-07-11 18:17:01 +0800

    modified:   test/USDTFork.t.sol

--- test/USDTFork.t.sol
+++ test/USDTFork.t.sol
@@ -1,5 +1,6 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
+
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "forge-std/Test.sol";
@@ -9,16 +10,23 @@ contract USDTForkTest is Test {
     IERC20 usdt = IERC20(0xdAC17F958D2ee523a2206206994597C13D831ec7);
     address alice;
     address bob = address(0x22);
+
     function setUp() external {
+        // 配置 fork url
+        uint256 forkId = vm.createFork("https://rpc.ankr.com/eth");
+        vm.selectFork(forkId);
+
         alice = address(0x11);
         uint256 before_ = usdt.totalSupply();
         deal(address(usdt), alice, 10000e6);
         uint256 after_ = usdt.totalSupply();
         assertEq(before_, after_);
     }
+
     function testBalanceOf() external {
         assertEq(usdt.balanceOf(alice), 10000e6);
     }
+
     function testTransfer() external {
         vm.startPrank(alice);
         usdt.safeTransfer(bob, 20);
@@ -28,3 +36,4 @@ contract USDTForkTest is Test {
 }
 
 // forge test -vvv --match-contract USDTForkTest --fork-url mainnet --block-number 2028232
+// 如果配置了 selectFork: forge test -vvv --match-contract USDTForkTest
